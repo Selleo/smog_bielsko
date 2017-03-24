0993bd123a6bb57cb02d692429a25ccc8aa56968
dpienczykowski <dpienczykowski@gmail.com>
2017-03-24 21:03:58 +0100

    setup index tests

---
+++ components/__tests__/index.android.spec.js
@@ -0,0 +1,25 @@
+import 'react-native';
+import React                          from 'react';
+
+import { shallow }                    from 'enzyme';
+import chai, { expect }               from 'chai';
+import dirtyChai                      from 'dirty-chai';
+
+import smog_bielsko                   from '../../index.android';
+import { data }                       from './mock/data'
+
+const originalExpect = global.expect;
+
+chai.use(dirtyChai);
+
+let wrapper, props;
+
+describe('smog_bielsko', () => {
+
+  it('renders correctly', () => {
+    wrapper = shallow(<smog_bielsko />);
+    originalExpect(wrapper).toMatchSnapshot();
+    expect(wrapper).to.exist();
+  });
+
+});
